c590ad5d234ddcd217e576a5ca377072bcf067c1
ULTIMATESLAUGHTER <aislas1@byupathway.edu>
2025-10-29 10:36:30 -0600

    adjusted svg image sizes for link usage. Added js functions for date last modified and current year.

--- week01/scripts/starting-index.js
+++ week01/scripts/starting-index.js
@@ -3,3 +3,26 @@ const navButton = document.querySelector('#nav-button');
 navButton.addEventListener('click', () => {
     navButton.classList.toggle('show');
 });
+
+const navBar = document.querySelector('#nav-bar');
+
+navButton.addEventListener('click', () => {
+    navBar.classList.toggle('show');
+    navButton.classList.toggle('active');
+});
+
+function currentyear() {
+    const currentYear = new Date().getFullYear();
+    document.getElementById("currentyear").textContent = currentYear;
+    return currentYear;
+}
+
+function lastModified() {
+    const lastModified = new Date(document.lastModified);
+    document.getElementById('lastModified').textContent = `${lastModified}`;
+}
+
+console.log('This file was last modified on: ${lastModified()}');
+
+currentyear();
+lastModified();
